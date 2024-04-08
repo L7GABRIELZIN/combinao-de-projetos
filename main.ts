b672339e23f73012845307ba962a6a330ca53a9c
L7GABRIELZIN <165018281+L7GABRIELZIN@users.noreply.github.com>
2024-04-08 13:38:53 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onGesture(Gesture.Shake, function () {
+    basic.showString("PARE ESTOU FICANDO TONTO!")
+    basic.showIcon(IconNames.Angry)
+    basic.showString("você vai continuar?")
+    basic.showIcon(IconNames.Asleep)
+})
+basic.forever(function () {
+	
+})
